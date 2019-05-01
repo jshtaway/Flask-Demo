458b5233b45b7bafa4681b2cf4cc9f70831c77df
zen-gineer <jshtaway@gmail.com>
2019-05-01 12:26:03 -0700

    changed home page label

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import (
     render_template,
     jsonify)
 
-  
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -30,47 +30,63 @@ session = Session(engine)
 #################################################
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'You\'re at the home page'
+    return jsonify('This is a simple REST API. Try these urls to get weather information:\
+            \
+            \
+            /api/v1.0/precipitation\
+                \
+                /api/v1.0/stations\
+                    \
+                    /api/v1.0/tobs\
+                        \
+                /api/v1.0/<start>/<end>')
+
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
-    dateList = [int(el) for el in max(session.query(Measurement.date).all()).date.split('-')]
+    dateList = [int(el) for el in max(session.query(
+        Measurement.date).all()).date.split('-')]
     earliestDate = dt.date(*dateList) - dt.timedelta(days=365)
-    results = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date > earliestDate)
+    results = session.query(Measurement.tobs, Measurement.date).filter(
+        Measurement.date > earliestDate)
     temperatures = {}
     for tobs, date in results:
         temperatures[str(date)] = str(tobs)
     return jsonify(temperatures)
+
 
 @app.route('/api/v1.0/stations')
 def stations():
     results = session.query(Station.station).all()
     return(jsonify(results))
 
+
 @app.route('/api/v1.0/tobs')
 def tobs():
     results = session.query(Measurement.tobs).all()
     return(jsonify(results))
 
+
 @app.route('/api/v1.0/<start>/<end>')
 def startEnd(start, end):
     start = dt.datetime.strptime(start, "%Y-%m-%d")
     if not end:
-        end = dt.datetime.today()#.strftime("%Y-%m-%d")
+        end = dt.datetime.today()  # .strftime("%Y-%m-%d")
     else:
         end = dt.datetime.strptime(end, "%Y-%m-%d")
-    #print(end)
-    results = session.query(Measurement.tobs).filter(Measurement.date > start).filter(Measurement.date < end).all()
-    print (type(results[0])
+    # print(end)
+    results = session.query(Measurement.tobs).filter(
+        Measurement.date > start).filter(Measurement.date < end).all()
+    print(type(results[0]))
     resultsList = []
     [results.append(r[0]) for r in results]
     mintemp = min(results)
     maxtemp = max(results)
     avetemp = sum(results)/len(results)
     return(jsonify([mintemp, maxtemp, avetemp]))
-
 
 
 if __name__ == '__main__':
